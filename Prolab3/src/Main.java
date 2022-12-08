6ab94d11c709533ad84bf5dfc612da9209453785
Muhammet Ali Yazıcı <muhammedaliali25@gmail.com>
2022-12-08 03:49:09 +0300

    dosya okuma denemesi

--- Prolab3/src/Main.java
+++ Prolab3/src/Main.java
@@ -1,8 +1,36 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Mali Yazici");
-        System.out.println("Gürkan Culfa");
+    public static void main(String[] args) throws FileNotFoundException {
+        readCSV();
     }
 
+    static public void readCSV() throws FileNotFoundException {
+        String file = "/Users/mali/Desktop/Prolab3/Prolab3/src/test2.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null){
+                String[] row = line.split(";");
+                for (String s : row) {
+                    System.out.printf("%-10s", s);
+                }
+                System.out.println();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    }
 }
